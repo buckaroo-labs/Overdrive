a754cfd1caf56f0963267749e9fad8b9b5df191f
buckaroo-labs <46908086+buckaroo-labs@users.noreply.github.com>
2024-01-05 14:29:23 -0800

    Update services.php

--- htdocs/services.php
+++ htdocs/services.php
@@ -188,6 +188,8 @@ if ($page_count>0) {
 </p></div>
 <!-- END MAIN -->
 </div>
-
-<?php include "Hydrogen/elemFooter.php"; ?>
+<?php
+	include 'Hydrogen/elemNavbar.php';
+	include "Hydrogen/elemFooter.php";
+?>
 </body></html>
